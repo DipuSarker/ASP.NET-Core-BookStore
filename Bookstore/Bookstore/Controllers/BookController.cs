beea228ecf74598b5c1b1caad7098d835ed0da1a
DipuSarker <mr.d.sarker@gmail.com>
2020-09-01 01:16:07 -0700

    Added libman to the client side repository

---
+++ Bookstore/Bookstore/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Bookstore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public ActionResult GetAllBook()
+        {
+            var data = _bookRepository.GetGetAllBook();
+            return View();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBook(string bookName, string authorName)
+        {
+            return _bookRepository.SearchBook(bookName, authorName);
+        }
+    }
+}
---
+++ Bookstore/Bookstore/Repository/BookRepository.cs
@@ -0,0 +1,37 @@
+﻿using Bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetGetAllBook()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById( int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) && x.Author.Contains(authorName)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1, Title="MVC", Author="Dipu"},
+                new BookModel(){Id=2, Title="MVC2", Author="Dipu2"},
+                new BookModel(){Id=3, Title="MVC3", Author="Dipu3"},
+                new BookModel(){Id=4, Title="MVC4", Author="Dipu4"}
+            };
+        }
+    }
+}
